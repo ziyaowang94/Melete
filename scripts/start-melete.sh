84169d0fbd23ad1803bbdd9c21650e7321a156e8
3120230991 <1440027762@qq.com>
2025-07-14 22:59:26 +0800

    1. Change Uranus to Melete;  2. Change Bridge Shards to Coordination Shards(beyond source)

---
+++ scripts/start-melete.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+target_folder=$1
+start_time=$2
+
+#start.sh
+
+for subfolder in "$target_folder"/*; do
+    if [ -d "$subfolder" ]; then  
+        (./melete --root="$subfolder" --start-time="$start_time" > "$subfolder"/.out 2>&1 &)
+    fi
+done
